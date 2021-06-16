92c6fcb608c71ce07fb3e456cf71b43e02112d57
rmalolin <steammushroom@gmail.com>
2021-06-16 12:05:53 +0300

    html / css / js

---
+++ app.js
@@ -0,0 +1,19 @@
+const slidesPlugin = (activeSlide = 1) => {
+
+    const slides = document.querySelectorAll('.slide')
+slides[activeSlide].classList.add('active')
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses()
+        slide.classList.add('active')
+    })
+}
+
+const clearActiveClasses = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')})
+}
+}
+
+slidesPlugin()
